a7386bc0e9fb5ce6ced49e9520de5d3e7675a5ab
Frank <frankli0324@hotmail.com>
2020-03-15 16:16:13 +0800

    [CTFd] notice shouldn't be parsed as json object

--- Modules/CTFd/NoticeMessage.cs
+++ Modules/CTFd/NoticeMessage.cs
@@ -8,26 +8,33 @@ namespace CyanBot.Modules.CTFdUtils {
         public static Message Message (Event e) {
             switch (e.type) {
             case "challenge":
-                return FromChallenge (JToken.Parse (e.data));
+                return FromChallenge (e.data);
             case "hint":
                 return null;
             case "ping":
                 logger.Debug ("ping from CTFd");
                 return null;
+            case "notification":
+                logger.Debug ("published notification");
+                return FromNotice (e.data);
             default:
                 logger.Warn ("unknown event type: " + e.type);
                 return null;
             }
         }
-        static Message FromChallenge(JToken data) {
-            switch (data["type"].ToObject<string> ()) {
-                case "challenge_solved":
-                return new Message ($"{data["user"]["name"]}做出了{data["challenge"]}");
-                case "challenge_created":
+        static Message FromChallenge (string data) {
+            var json = JToken.Parse (data);
+            switch (json["type"].ToObject<string> ()) {
+            case "challenge_solved":
+                return new Message ($"{json["user"]["name"]}做出了{json["challenge"]}");
+            case "challenge_created":
                 break;
-                case "challenge_updated":
+            case "challenge_updated":
                 break;
             }
+            return null;
+        }
+        static Message FromNotice (string data) {
             return null;
         }
     }
