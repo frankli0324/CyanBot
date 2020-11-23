a0fb127c91be8b010cb3f07c05db883b56d66a0c
Frank <frankli0324@hotmail.com>
2020-11-24 04:00:39 +0800

    [Common] dockerize app

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM mcr.microsoft.com/dotnet/runtime-deps:5.0-buster-slim-amd64
+
+COPY ./bin/Release/netcoreapp5.0/linux-x64/publish/[^C]* /app/
+COPY ./bin/Release/netcoreapp5.0/linux-x64/publish/C[^y]* /app/
+COPY ./bin/Release/netcoreapp5.0/linux-x64/publish/Cyan* /app/
+WORKDIR /opt
+
+ENV TZ=Asia/Shanghai
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+CMD [ "/app/CyanBot" ]
+VOLUME [ "/opt" ]
--- Program.cs
+++ Program.cs
@@ -1,48 +1,34 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Threading;
 using cqhttp.Cyan.Clients;
 using cqhttp.Cyan.Events.CQEvents.Base;
 using cqhttp.Cyan.Events.CQResponses;
-using Newtonsoft.Json.Linq;
 
 namespace CyanBot {
     class Program {
         public static CQApiClient client;
         public static Dictionary<string, string> Globals =
             new Dictionary<string, string> ();
-        static void LoadCfg () {
-            if (File.Exists ("config.json")) {
-                string config = File.ReadAllText ("config.json");
-                try {
-                    var config_parsed = JToken.Parse (config);
-                    var coolq_config = config_parsed["coolq_config"];
-                    Globals = config_parsed["globals"].ToObject<Dictionary<string, string>> ();
-                    client = new CQHTTPClient (
-                        access_url: coolq_config["api_addr"].ToObject<string> (),
-                        listen_port: coolq_config["listen_port"].ToObject<int> (),
-                        access_token: coolq_config["access_token"].ToObject<string> (),
-                        secret: coolq_config["listen_secret"].ToObject<string> (),
-                        use_group_table: true
-                    );
-                } catch {
-                    throw new Exception ("Config file parse error");
-                }
-                return;
-            }
-            throw new ArgumentNullException ("缺乏正常启动所需的配置文件或参数");
-        }
+        public static readonly LiteDB.LiteDatabase Data =
+            new LiteDB.LiteDatabase ("Filename=cyan.db");
         static void Main (string[] args) {
-            LoadCfg ();
+            foreach (System.Collections.DictionaryEntry obj in Environment.GetEnvironmentVariables ())
+                Globals.Add ((string) obj.Key, (string) obj.Value);
+            client = new CQWebsocketClient (
+                access_url: Globals["access_url"],
+                event_url: Globals["event_url"],
+                access_token: Globals["access_token"]
+            );
+            cqhttp.Cyan.Utils.Logger.GetLogger ("cqhttp.Cyan").log_level = cqhttp.Cyan.Enums.Verbosity.ERROR;
+            cqhttp.Cyan.Utils.Logger.GetLogger ("modules").log_level = cqhttp.Cyan.Enums.Verbosity.INFO;
+
             client.OnEventAsync += async (cli, e) => {
-                if (e is MessageEvent) {
-                    await Dispatcher.Dispatcher.Dispatch (
-                        cli, e as MessageEvent
-                    );
-                }
+                if (e is MessageEvent mevent)
+                    await Dispatcher.Dispatcher.Dispatch (cli, mevent);
                 return new EmptyResponse ();
             };
-            Console.ReadLine ();
+            Thread.Sleep (Timeout.Infinite);
         }
     }
 }
